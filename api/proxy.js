8cf84576e1e732c8c563f7a8b0cb75e96ba57cde
xtoazt <183557618+xtoazt@users.noreply.github.com>
2025-06-19 17:29:43 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/proxy.js
@@ -0,0 +1,23 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = (req, res) => {
+  let target = "https://www.google.com/";//your website url
+  //   if (
+  //     req.url.startsWith("/api") ||
+  //     req.url.startsWith("/auth") ||
+  //     req.url.startsWith("/banner") ||
+  //     req.url.startsWith("/CollegeTask")
+  //   ) {
+  //     target = "http://106.15.2.32:6969";
+  //   }
+
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      // rewrite request path `/backend`
+      //  /backend/user/login => http://google.com/user/login
+      //   "^/backend/": "/",
+    },
+  })(req, res);
+};
